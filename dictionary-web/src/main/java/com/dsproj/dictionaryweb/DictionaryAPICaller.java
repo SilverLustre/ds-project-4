54f9d224e6d9715fafa5824fe03f900d37a7f341
Jiahe Tian <jiahet@andrew.cmu.edu>
2022-11-12 15:50:55 -0500

    dictionary call add log function;

--- dictionary-web/src/main/java/com/dsproj/dictionaryweb/DictionaryAPICaller.java
+++ dictionary-web/src/main/java/com/dsproj/dictionaryweb/DictionaryAPICaller.java
@@ -1,9 +1,7 @@
 package com.dsproj.dictionaryweb;//package com.mkyong.http;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.sun.jndi.toolkit.url.UrlUtil;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -12,6 +10,15 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+class QueryResult {
+    public String origin;
+    public String formatted;
+
+    public QueryResult(String origin, String formatted) {
+        this.origin = origin;
+        this.formatted = formatted;
+    }
+}
 
 public class DictionaryAPICaller {
 
@@ -23,51 +30,59 @@ public class DictionaryAPICaller {
         httpClient = new OkHttpClient();
     }
 
-    public String sendGETSync(String input) {
+    public QueryResult sendGetURL(String url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "Token 4cc71b234ded51f7265672fc7060445954125471")  // add request headers
+                .build();
+        String json = null;
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            json = response.body().string();
+            System.out.println(json);
+            JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+            JsonObject definitionJsonObj = jsonObject.get("definitions").getAsJsonArray().get(0).getAsJsonObject();
+            String definition = "";
+            if (definitionJsonObj.has("definition") && !definitionJsonObj.get("definition").isJsonNull()) {
+                definition = definitionJsonObj.get("definition").getAsString();
+            }
+            String imageUrl = "";
+            if (definitionJsonObj.has("image_url") && !definitionJsonObj.get("image_url").isJsonNull()) {
+                imageUrl = definitionJsonObj.get("image_url").getAsString();
+            }
+
+            JsonObject responseObj = new JsonObject();
+            responseObj.addProperty("definition", definition);
+            responseObj.addProperty("image_url", imageUrl);
+//            return responseObj.toString();
+            return new QueryResult(json, responseObj.toString());
+        } catch (java.net.UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        return new JsonObject().toString();
+        return new QueryResult(json, new JsonObject().toString());
+    }
+
+    public QueryResult sendGet(String input) {
         try {
             String url = "https://owlbot.info/api/v4/dictionary/" + URLEncoder.encode(input, "UTF-8");
-            Request request = new Request.Builder()
-                    .url(url)
-                    .addHeader("Authorization", "Token 4cc71b234ded51f7265672fc7060445954125471")  // add request headers
-                    .build();
-            try (Response response = httpClient.newCall(request).execute()) {
-                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-                String json = response.body().string();
-                System.out.println(json);
-                JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
-                JsonObject definitionJsonObj = jsonObject.get("definitions").getAsJsonArray().get(0).getAsJsonObject();
-                String definition = "";
-                if (definitionJsonObj.has("definition")&&!definitionJsonObj.get("definition").isJsonNull()) {
-                    definition = definitionJsonObj.get("definition").getAsString();
-                }
-                String imageUrl = "";
-                if (definitionJsonObj.has("image_url")&&!definitionJsonObj.get("image_url").isJsonNull()) {
-                    imageUrl = definitionJsonObj.get("image_url").getAsString();
-                }
-
-                JsonObject responseObj = new JsonObject();
-                responseObj.addProperty("definition", definition);
-                responseObj.addProperty("image_url", imageUrl);
-                return responseObj.toString();
-            } catch (java.net.UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            return sendGetURL(url);
 
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
 
-        return new JsonObject().toString();
     }
 
     public static void main(String[] args) throws IOException {
         DictionaryAPICaller obj = new DictionaryAPICaller();
-        String res = obj.sendGETSync("apple and banana");
-        System.out.println(res);
+        QueryResult res = obj.sendGet("apple and banana");
+        System.out.println(res.formatted);
         System.out.println("ok");
     }
 
--- dictionary-web/src/main/java/com/dsproj/dictionaryweb/DictionaryServlet.java
+++ dictionary-web/src/main/java/com/dsproj/dictionaryweb/DictionaryServlet.java
@@ -1,31 +1,94 @@
 package com.dsproj.dictionaryweb;
 
+import com.google.gson.Gson;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.bson.Document;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.time.Instant;
+import java.util.Date;
 
 @WebServlet(name = "dictionaryServlet", value = "/dictionary")
 public class DictionaryServlet extends HttpServlet {
+    MongoDatabase database;
 
     public void init() {
-
+        String mongoPass = System.getenv("MONGO_PASS");
+        ConnectionString connectionString = new ConnectionString(String.format("mongodb+srv://owlat:%s@cluster0.lylnbco.mongodb.net/?retryWrites=true&w=majority", mongoPass));
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .serverApi(ServerApi.builder()
+                        .version(ServerApiVersion.V1)
+                        .build())
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        database = mongoClient.getDatabase("owlat");
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+        MongoCollection<Document> log = database.getCollection("aggregated_log");
         String input = request.getParameter("input");
         System.out.println("Get input:" + input);
 
+        String model = request.getParameter("model");
+        System.out.println("Get model:" + model);
+
         // response
         response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
+        Date preDT = new Date();
         DictionaryAPICaller dictionaryAPICaller = new DictionaryAPICaller();
-        String jsonResult = dictionaryAPICaller.sendGETSync(input);
-        out.println(jsonResult);
+        String url = "https://owlbot.info/api/v4/dictionary/" + URLEncoder.encode(input, "UTF-8");
+        QueryResult queryResult = dictionaryAPICaller.sendGetURL(url);
+        Date postDT = new Date();
+//        new Thread(()->{
+//            log.insertOne(new Document()
+//                    .append("input", input)
+//                    .append("model", model)
+//                    .append("start_time", preDT)
+//                    .append("end_time", postDT)
+//                    .append("duration", (postDT.getTime() - preDT.getTime())/1000.0)
+//                    .append("request_from_user", new Document()
+//                            .append("input", input)
+//                            .append("model", model))
+//                    .append("request_to_api", new Document()
+//                            .append("get_url", url))
+//                    .append("response_from_api", new Document()
+//                            .append("origin", Document.parse(queryResult.origin)))
+//                    .append("response_to_user", new Document()
+//                            .append("formatted", Document.parse(queryResult.formatted)))
+//            );
+//        }).start();
+        log.insertOne(new Document()
+                .append("input", input)
+                .append("model", model)
+                .append("start_time", preDT)
+                .append("end_time", postDT)
+                .append("duration", (postDT.getTime() - preDT.getTime())/1000.0)
+                .append("request_from_user", new Document()
+                        .append("input", input)
+                        .append("model", model))
+                .append("request_to_api", new Document()
+                        .append("get_url", url))
+                .append("response_from_api", new Document()
+                        .append("origin", Document.parse(queryResult.origin)))
+                .append("response_to_user", new Document()
+                        .append("formatted", Document.parse(queryResult.formatted)))
+        );
+        PrintWriter out = response.getWriter();
+        out.println(queryResult.formatted);
     }
 
     public void destroy() {
